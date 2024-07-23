0ce9203ec247df677841eebdfcf21cacb0b649d2
Sangjun Lee <112681633+lsjmc0224@users.noreply.github.com>
2024-07-23 20:24:25 +0900

    Title: 정수 사각형 최대 합, Time: 157ms, Memory: 25MB, Status: Passed - CodeTree

--- 240723/정수 사각형 최대 합/maximum-sum-path-in-square.py
+++ 240723/정수 사각형 최대 합/maximum-sum-path-in-square.py
@@ -7,8 +7,8 @@ def dp(grid, n):
 
     for i in range(1, n):
         arr[i][0] = arr[i-1][0] + grid[i][0]
-    for j in range(1, n):
-        arr[0][j] = arr[0][j-1] + grid[0][j]
+        arr[0][i] = arr[0][i-1] + grid[0][i]
+        
     for i in range(1, n):
         for j in range(1, n):
             arr[i][j] = max(arr[i - 1][j] + grid[i][j], arr[i][j - 1] + grid[i][j])
